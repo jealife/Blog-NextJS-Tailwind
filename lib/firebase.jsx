995a2df692b9213314cc782b98f18bef50497dc4
Jealife <jeanguylanememiaghe@gmail.com>
2024-08-15 04:07:33 +0100

    Changes to be committed: 	modified:   lib/firebase.jsx

--- lib/firebase.jsx
+++ lib/firebase.jsx
@@ -1,22 +1,22 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 import {getAut, getAuth} from 'firebase/auth';
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDLLpLuJlHgv7UfjkFi6oValBLLtHCESus",
-//   authDomain: "bloggg-8cd30.firebaseapp.com",
-//   projectId: "bloggg-8cd30",
-//   storageBucket: "bloggg-8cd30.appspot.com",
-//   messagingSenderId: "452758491349",
-//   appId: "1:452758491349:web:3bdd602f5dfe0bfa28d25c",
-// };
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDING_ID,
-  appId:process.env.APP_ID,
+  apiKey: "AIzaSyDLLpLuJlHgv7UfjkFi6oValBLLtHCESus",
+  authDomain: "bloggg-8cd30.firebaseapp.com",
+  projectId: "bloggg-8cd30",
+  storageBucket: "bloggg-8cd30.appspot.com",
+  messagingSenderId: "452758491349",
+  appId: "1:452758491349:web:3bdd602f5dfe0bfa28d25c",
 };
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDING_ID,
+//   appId:process.env.APP_ID,
+// };
 const app = initializeApp(firebaseConfig);
 
 export const db= getFirestore();
