63b1efe1bd7a48e2aad7cc119ef8f635b7845fea
Jealife <jeanguylanememiaghe@gmail.com>
2024-08-15 12:48:07 +0100

    modified:   app/components/Header/LoginButton.jsx 	deleted:    public/Google.webp 	deleted:    public/Logo.png 	deleted:    public/blogg.png

--- app/components/Header/LoginButton.jsx
+++ app/components/Header/LoginButton.jsx
@@ -13,7 +13,7 @@ export default function LoginButton() {
   }
   if(user){
     return(
-      <div className="flex items-center gap-5 flex-row-reverse justify-end">
+      <div className="flex items-center gap-3 flex-row-reverse justify-end">
         <button className="cursor-pointer flex items-center gap-1 bg-[#1B202A]/1 text-[#1B202A] rounded-full px-0 py-0 hover:text-[#d12c0f] duration-300 "
         title="Logout"
         aria-label="Logout"
@@ -23,9 +23,10 @@ export default function LoginButton() {
         >
           <LogOut/>
         </button>
-        <span className="text-sm text-right">
-          {user?.displayName}
-        </span>
+        <div className="text-sm text-right flex">
+          {/* {user?.displayName} */}
+          <img src={user?.photoURL} alt={user?.displayName} className="h-10 w-10 object-cover rounded-full" />
+        </div>
       </div>
     )
   }
@@ -35,7 +36,7 @@ export default function LoginButton() {
       onClick={()=>{
         handleSignInWithGoogle();
       }}
-       className="flex items-center gap-2 bg-[#1B202A]/90 text-white rounded-full px-4 py-2 hover:bg-[#0F0FD1] duration-300 ">
+       className="text-xs flex items-center gap-2 bg-[#1B202A]/90 text-white rounded-full px-4 py-2 hover:bg-[#0F0FD1] duration-300 ">
         <img className="w-7" src="https://firebasestorage.googleapis.com/v0/b/bloggg-8cd30.appspot.com/o/Google.webp?alt=media&token=f95d6e45-26bb-440b-83bf-acd750fa0005" alt="" />
         Login with google
       </button>
