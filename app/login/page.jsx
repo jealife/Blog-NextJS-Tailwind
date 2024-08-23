add243aa67ceb6cd69b0fc1f7f66c4dafde65d3c
Jealife <jeanguylanememiaghe@gmail.com>
2024-08-24 00:55:34 +0100

    Changes to be committed: 	modified:   app/login/page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,9 +5,9 @@ import LoginForm from "./LoginForm";
 
 export default function page() {
   return (
-    <main className="flex flex-col items-center justify-center h-[60svh] w-full">
-      <div className="bg-white rounded-md shadow-xl h-48 max-w-96 w-full flex flex-col gap-4 items-center justify-center  px-2">
-        <h2 className="font-semibold text-2xl">Connectez-vous avec Google</h2>
+    <main className="flex flex-col items-center justify-center h-[60svh] px-3 w-full">
+      <div className="bg-white rounded-md shadow-xl h-48 max-w-96 w-full flex flex-col gap-4 items-center justify-center  px-4">
+        <h2 className="font-semibold text-2xl text-center">Connectez-vous avec Google</h2>
         <LoginForm />
       </div>
     </main>
